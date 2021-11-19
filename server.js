316ec4b4e7af8423e68bd20f1161e5a298a4834a
Abhay Zala <54415911+aszala-UNC@users.noreply.github.com>
2021-11-18 22:38:08 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use("/app/user/:id", (req, res) => {
 	const id = req.params.id;
 
 	const stmt = db.prepare(`SELECT * FROM userinfo WHERE id = ?`).run(id);
-	res.status(200).json({"message": `1 record updated: ID ${stmt.lastInsertRowid} (200)`});
+	res.status(200).json(stmt[0]);
 });
 
 app.patch("/app/update/:id", (req, res) => {
@@ -65,9 +65,8 @@ app.patch("/app/update/:id", (req, res) => {
 	const pass = md5(req.body.pass);
 
 	const stmt = db.prepare(`UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?`).run(user, pass, id);
-	res.status(200).json({"message": `1 record deleted: ID ${stmt.lastInsertRowid} (200)`});
+	res.status(200).json({"message": `1 record updated: ID ${stmt.lastInsertRowid} (200)`});
 });
-
 
 app.delete("/app/delete/user/:id", (req, res) => {
 	const id = req.params.id;
